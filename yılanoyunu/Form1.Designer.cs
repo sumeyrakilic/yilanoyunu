c175a400e77d0bb3839941beb994c24d3f6b7a68
sumeyrakilic <sumeyrakilicedu@gmail.com>
2026-04-25 22:50:51 +0300

    Add project files.

---
+++ yılanoyunu/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace yılanoyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.puan = new System.Windows.Forms.Label();
+            this.skor = new System.Windows.Forms.Label();
+            this.tebriks = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // puan
+            // 
+            this.puan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puan.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.puan.ForeColor = System.Drawing.Color.White;
+            this.puan.Location = new System.Drawing.Point(675, 55);
+            this.puan.Name = "puan";
+            this.puan.Size = new System.Drawing.Size(142, 59);
+            this.puan.TabIndex = 1;
+            this.puan.Text = "PUAN: ";
+            this.puan.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // skor
+            // 
+            this.skor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.skor.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skor.ForeColor = System.Drawing.Color.White;
+            this.skor.Location = new System.Drawing.Point(697, 114);
+            this.skor.Name = "skor";
+            this.skor.Size = new System.Drawing.Size(105, 59);
+            this.skor.TabIndex = 2;
+            this.skor.Text = "0";
+            this.skor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.skor.Click += new System.EventHandler(this.skor_Click);
+            // 
+            // tebriks
+            // 
+            this.tebriks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.tebriks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tebriks.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tebriks.ForeColor = System.Drawing.Color.Black;
+            this.tebriks.Location = new System.Drawing.Point(628, 368);
+            this.tebriks.Name = "tebriks";
+            this.tebriks.Size = new System.Drawing.Size(253, 78);
+            this.tebriks.TabIndex = 3;
+            this.tebriks.Text = "TEBRİKLER :)";
+            this.tebriks.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.tebriks.Click += new System.EventHandler(this.tebriks_Click);
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.start.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.start.ForeColor = System.Drawing.Color.Black;
+            this.start.Location = new System.Drawing.Point(675, 472);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(148, 59);
+            this.start.TabIndex = 5;
+            this.start.Text = "START";
+            this.start.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(893, 625);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.tebriks);
+            this.Controls.Add(this.skor);
+            this.Controls.Add(this.puan);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label puan;
+        private System.Windows.Forms.Label skor;
+        private System.Windows.Forms.Label tebriks;
+        private System.Windows.Forms.Label start;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ yılanoyunu/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yılanoyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Panel body_part;
+        Panel nuts = new Panel();
+        List<Panel> yilan = new List<Panel>();
+
+        string yon = "sag";
+        
+        private void start_Click(object sender, EventArgs e)
+        {
+            skor.Text ="0";
+            panelSil();
+            body_part = new Panel();
+            body_part.Location = new Point(200, 200);
+            body_part.Size = new Size(20, 20);
+            body_part.BackColor = Color.Gray;
+            yilan.Add(body_part);
+            panel1.Controls.Add(yilan[0]);
+
+            timer1.Start();
+            nutsOlustur();
+
+            
+        }
+
+        void carptımı()
+        {
+            for(int i = 2; i < yilan.Count; i++)
+            {
+                if (yilan[0].Location == yilan[i].Location)
+                {
+                    tebriks.Visible = true;
+                    tebriks.Text = "KAYBETTİN :(";
+                    timer1.Stop();
+
+                }
+            }
+        }
+
+        void panelSil()
+        {
+            panel1.Controls.Clear();
+            yilan.Clear();
+            tebriks.Visible= false;
+        }
+
+        void puanKontrol()
+        {
+            int puan = int.Parse (skor.Text);
+            if(puan > 500)
+            {
+                tebriks.Text = "TEBRİKLER :)";
+                tebriks.Visible = true;
+                timer1.Stop();
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void skor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void nutsOlustur()
+        {
+            Random rnd = new Random();
+            int nutsx, nutsy;
+            nutsx = rnd.Next(580);
+            nutsy = rnd.Next(580);
+
+            nutsx -= nutsx % 20;
+            nutsy -= nutsy % 20;
+
+            nuts.Size = new Size(20, 20);
+            nuts.BackColor = Color.Red;
+            nuts.Location = new Point(nutsx, nutsy);
+            panel1.Controls.Add(nuts);
+
+        }
+
+        void isnutseat()
+        {
+            int puan = int.Parse(skor.Text);
+            if (yilan[0].Location == nuts.Location)
+            {
+                panel1.Controls.Remove(nuts);
+
+                puan += 5;
+                skor.Text = puan.ToString();
+                nutsOlustur();
+                addbody();
+            }
+           
+        }
+
+        void addbody()
+        {
+            Panel ekbody = new Panel();
+            ekbody.Size = new Size(20, 20);
+            ekbody.BackColor = Color.Yellow;
+            yilan.Add(ekbody);
+            panel1.Controls.Add(ekbody);
+        }
+
+        void hareket()
+        {
+            for (int i = yilan.Count - 1; i > 0; i--)
+            {
+                yilan[i].Location = yilan[i - 1].Location;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+                yon = "sag"; 
+            if (e.KeyCode == Keys.Left)
+                yon = "sol";
+            if (e.KeyCode == Keys.Up)
+                yon = "yukari";
+            if (e.KeyCode == Keys.Down)
+                yon = "asagi";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int locx = yilan[0].Location.X;
+            int locy = yilan[0].Location.Y;
+
+            isnutseat();
+            hareket();
+            carptımı();
+            puanKontrol();
+
+            if(yon == "sag")
+            {
+                if(locx < 580)
+                {
+                    locx += 20;
+                }
+                else
+                {
+                    locx = 0;
+                }
+            }
+
+            if (yon == "sol")
+            {
+                if (locx > 0 )
+                {
+                    locx -= 20;
+                }
+                else
+                {
+                    locx = 580;
+                }
+            }
+
+            if (yon == "asagi")
+            {
+                if (locy < 580)
+                {
+                    locy += 20;
+                }
+                else
+                {
+                    locy = 0;
+                }
+            }
+
+            if (yon == "yukari")
+            {
+                if (locy > 0 )
+                {
+                    locy -= 20;
+                }
+                else
+                {
+                    locy = 580;
+                }
+            }
+            yilan[0].Location = new Point(locx, locy);
+        }
+
+        private void tebriks_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
